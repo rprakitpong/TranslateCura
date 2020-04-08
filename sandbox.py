13bc64dbb04ff17cdca15c7fb0c0ed1b2bf8d64b
rprakitpong <rprakitpong@gmail.com>
2020-04-08 16:56:39 -0700

    working, no comments

--- sandbox.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-#from translate import Translator
-
-t = Translator()
-s = t.translate('haha',dest='th')
-if s:
-    print(s.text)
--- translate.py
+++ translate.py
@@ -3,57 +3,61 @@ import polib
 import os
 import json
 
-print(os.getcwd())
-
-potFiles = ['cura.pot']
-#potFiles = ['cura.pot', 'fdmextruder.def.json.pot', 'fdmprinter.def.json.pot', 'uranium.pot']
-poExtension = '.po'
-moExtension = '.mo'
-poFiles = []
-moFiles = []
-for toRename in potFiles:
-    pre, ext = os.path.splitext(toRename)
-    newName = pre + poExtension
-    if os.path.exists(toRename):
-        os.rename(toRename, newName)
-        print(toRename + 'successfully renamed to ' + newName + '.')
+files = [('cura.pot','cura.po','cura.mo','cura.json'), ('fdmextruder.def.json.pot','fdmextruder.def.json.po','fdmextruder.def.json.mo','fdmextruder.def.json.json'), ('fdmprinter.def.json.pot','fdmprinter.def.json.po','fdmprinter.def.json.mo','fdmprinter.def.json.json'), ('uranium.pot','uranium.po','uranium.mo','uranium.json')]
+# pot, po, mo, json
+for _file in files:
+    potFile = _file[0]
+    poFile = _file[1]
+    jsonFile = _file[3]
+    if os.path.exists(potFile):
+        os.rename(potFile, poFile)
+        print(potFile + 'successfully renamed to ' + poFile + '.')
     else:
-        if not os.path.exists(newName):
-            print(pre + ' doesnt exist in directory. Please add ' + toRename + ' into directory before continuing.')
+        if not os.path.exists(poFile):
+            print(potFile + ' doesnt exist in directory. Please add directory before continuing.')
             exit()
         else:
-            print(newName + ' already exist in directory.')
-    poFiles.append(newName)
-    moFiles.append(pre + moExtension)
+            print(poFile + ' already exist in directory.')
+    if not os.path.exists(jsonFile):
+        with open(jsonFile,'w') as wf:
+            json.dump({" ":" "}, wf)
+
 
 translator = Translator()
 destLang = 'th'
 
-for ind in range(len(poFiles)):
-    poFile = poFiles[ind]
-    moFile = moFiles[ind]
+for _file in files:
+    poFile = _file[1]
+    moFile = _file[2]
+    jsonFile = _file[3]
 
     print('Translating ' + poFile + '...')
     po = polib.pofile(poFile)
 
+    enToDestLang = dict()
+    with open(jsonFile, 'r') as fp:
+        enToDestLang = json.load(fp)
+
     textsEN = [[]]
     currInd = 0
-    currIndCharCount = 0
-    maxCharCount = 12000 # 15000 is limit, but we go a little under
-    print("Current EN text array: " + str(currInd))
+    maxLen = 50 # trial and error, you get about 300 translations total
+    # maxLen, # of arrays, total
+    # 20, 15, 300 
+    # 30, 11, 330
+    # 40, 9, 360
+    #print("Current EN text array: " + str(currInd))
     for entry in po:
-        if currIndCharCount > maxCharCount:
-            currInd = currInd + 1
-            currIndCharCount = 0
-            textsEN.append([entry.msgid])
-            print("Current EN text array: " + str(currInd))
-        else:
-            textsEN[currInd].append(entry.msgid)
-            currIndCharCount = currIndCharCount + len(entry.msgid)
+        if entry.msgid not in enToDestLang:
+            #print(entry.msgid)
+            if len(textsEN[currInd]) > maxLen:
+                currInd = currInd + 1
+                textsEN.append([entry.msgid])
+                #print("Current EN text array: " + str(currInd))
+            else:
+                textsEN[currInd].append(entry.msgid)
         #print("Appended: " + entry.msgid)
     
     print("Start translating.")
-    enToDestLang = dict()
     try:
         for arrayInd, arrayVal in enumerate(textsEN): 
             print("Translating array number: " + str(arrayInd))
@@ -70,10 +74,9 @@ for ind in range(len(poFiles)):
             exit()
     
     print("Translation done. Saving backup.")
-    print(enToDestLang)
-    backup = open(poFile + ".txt", "w")
-    backup.write(str(enToDestLang))
-    backup.close()
+    #print(enToDestLang)
+    with open(jsonFile, 'w') as wf:
+        json.dump(enToDestLang, wf)
 
     print("Editting po file.")
     for entry in po:
